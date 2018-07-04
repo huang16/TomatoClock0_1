466d865a790676ccb150d5dfefdbea60c303fa7c
huang16 <1642387417@qq.com>
2018-07-04 17:54:40 +0800

    Add files via upload

---
+++ holyshit/Qml1Form.ui.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.4
+
+Item {
+    width: 960
+    height: 640
+}
+Text{
+    text:"helloworld"
+}
---
+++ holyshit/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_action1_1_triggered()
+{
+    QMessageBox::information(this,"fuck","fuckyou");
+}
